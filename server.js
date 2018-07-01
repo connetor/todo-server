d8a5cc006cfebfcee9cfa0ac726b7b83bc51095d
connetor <filmmieplern@gmail.com>
2018-07-01 11:07:50 +0700

    แก้ พอร์ต

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const router = jsonServer.router("db/db.json");
 const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
-server.listen(3001, () => {
+server.listen(3000, () => {
 console.log("JSON Server is running");
 });
